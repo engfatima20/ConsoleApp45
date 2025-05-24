ea256132f43e523482a064dd6987cc87c622b829
NV <NV@DESKTOP-832LD6A>
2025-05-24 16:47:03 +0300

    Add project files.

---
+++ ConsoleApp45/Program.cs
@@ -0,0 +1,65 @@
+﻿namespace ConsoleApp45
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the first num :");
+            int firstnum = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter the Operation please :");
+            char OP = char.Parse(Console.ReadLine());
+            Console.WriteLine("Enter the second num :");
+            int secondnum = int.Parse(Console.ReadLine());
+
+            int result =0;
+            bool valid = true;
+            if ( OP == '+')
+            {
+                result = firstnum + secondnum;
+            }
+            else if (OP == '-')
+            {
+                result = firstnum - secondnum;
+            }
+            else if (OP == '*')
+            {
+                result = firstnum * secondnum;
+            }
+            else if (OP == '/')
+            {
+                if (secondnum != 0)
+                {
+                    result = firstnum / secondnum;
+                } else
+                {
+                    Console.WriteLine("Error : Divide By Zero !");
+                    valid = false;
+                }
+            }
+            else if (OP == '%')
+            { 
+                if ( secondnum != 0) 
+                {
+                    result = firstnum % secondnum;
+                } else
+                {
+                    Console.WriteLine("Error : Divide By Zero !");
+                    valid = false;
+                }
+               
+            }
+            else
+            {
+                Console.WriteLine("Invalid Operator");
+                valid = false;
+            }
+            if (valid)
+            {
+                Console.WriteLine("Result = " + result);
+            }
+
+            
+           
+        }
+    }
+}
